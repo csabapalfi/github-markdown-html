01a1d8ea195c5f8882c792aeb59d33b1838119dd
Csaba Palfi <csaba@palfi.me>
2016-12-31 11:41:42 +0000

    (build) fix tests

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
-const spawn = require('child_process').spawnSync;
 const { readFileSync } = require('fs');
 
 test('renders index.html from README', () => {
-  const result = spawn('github-markdown-html', [], {stdio: 'inherit'});
   expect(readFileSync('index.html', 'utf-8')).toMatchSnapshot();
 });
